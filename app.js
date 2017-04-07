aff3b8aa2ef5875aa7cece8a13b89c72842207cb
Bryan Scott <bryan@pajato.com>
2017-04-07 19:27:02 -0400

    Got one form working both writing to and reading from the firebase database. Hooray! We can now deploy once CIMS is back up.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const config = {
     databaseURL: "https://dragons-hoard.firebaseio.com",
     projectId: "dragons-hoard",
     storageBucket: "dragons-hoard.appspot.com",
-    //messagingSenderId: "685172894729"
+    messagingSenderId: "685172894729"
 };
 firebase.initializeApp(config);
 //const firebaseDatabase = firebase.database();
@@ -21,6 +21,8 @@ firebase.initializeApp(config);
 // Ensure we get our schemas from db.js
 const db = require('./db.js');
 const app = express();
+
+const Sheet = mongoose.model('Sheet');
 
 // 1. Express-Static
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,7 +35,7 @@ app.use(bp.urlencoded({extended: false}));
 // 4. Routing
 
 app.get('/', function(req, res) {
-    const user = req.body.user;
+    //const user = req.body.user;
     res.render('home')
 });
 
@@ -53,15 +55,50 @@ app.get('/sheets', function(req, res) {
     res.render('sheets');
 });
 
-app.get('/sheets/newSheet', function(req, res) {
-   res.render('sheet', null);
+app.post('/sheet/newSheet', function (req, res) {
+    console.log('uid', req.body.huid);
+    //console.log('access token', req.body.hact);
+    console.log('name', req.body.sheetName);
+
+    if(req.body.sheetName + '' === '') {
+        res.render('sheets', {error: 'You must enter a sheet name.'});
+        return;
+    }
+
+    //firebase.auth().signInWithCustomToken(req.body.hact);
+
+    const key = firebase.database().ref().child('sheets').push().key;
+    const sheet = new Sheet();
+    sheet.name = req.body.sheetName;
+    sheet.level = 1;
+    sheet.slug = key;
+    sheet.class = 'Adventurer';
+
+    const updates = {};
+    updates['/users/' + req.body.huid + '/' + key] = key;
+    updates['/sheets/' + key] = {name: sheet.name, level: sheet.level, slug: key, class: sheet.class};
+    firebase.database().ref().update(updates).then(function() {
+        res.render('sheet', {sheet: sheet});
+    });
 });
 
-app.get('/sheets/:slug', function(req, res) {
-    const slug = req.body.slug;
-    // TODO: Get the correct sheet from the database using the slug.
-    let sheet = {};
-    res.render('sheet', sheet);
+app.get('/sheet/newSheet', function(req, res) {
+   res.render('slug', null);
+});
+
+app.get('/sheet/:slug', function(req, res) {
+    const slug = req.params.slug;
+
+    let sheet = new Sheet();
+    firebase.database().ref('/sheets/' + slug).once('value', function(snapshot) {
+        sheet.name = snapshot.val().name;// + ' level ' + snapshot.val().level + ' ' + snapshot.val().class;
+        sheet.level = snapshot.val().level;
+        sheet.slug = snapshot.val().slug;
+        sheet.class = snapshot.val().class;
+    }).then(function() {
+        res.render('slug', {sheet: sheet});
+    });
+
 });
 
 app.listen(3000);
